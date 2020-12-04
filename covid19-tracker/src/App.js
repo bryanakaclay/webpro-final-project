0cdda9e0588c698d3a6a1280a9e108fa7c1623d7
bryanakaclay <js.bryann@gmail.com>
2020-12-04 17:22:18 +0800

    Final-Detailing-3

--- covid19-tracker/src/App.js
+++ covid19-tracker/src/App.js
@@ -5,28 +5,30 @@ import {International, Local, Province} from './Components'
 
 const App = () =>{
   return(
+    <div className="main">
     <Router>
       <div>
         <div className="header">
           <h1>COVID-19 Tracker</h1>
           <p>"Badiam sadiki napa dirumah? ¯\_(ツ)_/¯" -Manado's Language</p>
+          <p>Please click at link below to see COVID-19 data case.</p>
         </div>
         <div className="main-nav-container">
           <nav align="center">
             <table align="center">
               <tr className="link1">
                 <th className="link1">
-                  <Link to="/International">
+                  <Link to="/international">
                     International
                   </Link>
                 </th>
                 <th className="link2">
-                  <Link to="/Local">
+                  <Link to="/local">
                     Local
                   </Link>
                 </th>
                 <th className="link3">
-                  <Link to="/Province">
+                  <Link to="/province">
                     Province
                   </Link>
                 </th>
@@ -47,6 +49,7 @@ const App = () =>{
         </Route>
       </Switch>
     </Router>
+    </div>
   )
 }
 
