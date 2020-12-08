855f5baf2ce5f1c4d251747b801f4bcd01390e23
bryanakaclay <js.bryann@gmail.com>
2020-12-08 11:02:24 +0800

    Final-Commit

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,7 +1,6 @@
 import Header from './Header'
-import Navigation from './Navigation'
 import International from './International'
 import Local from './Local'
 import Province from './Province'
 
-export {Header, Navigation, International, Local, Province}
+export {Header, International, Local, Province}
