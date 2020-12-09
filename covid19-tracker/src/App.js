1fb429c212e370e0c3eb46c9528bfed865927900
bryanakaclay <js.bryann@gmail.com>
2020-12-10 01:21:00 +0800

    Fixing-Error-1

--- covid19-tracker/src/App.js
+++ covid19-tracker/src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+import {Header, International, Local, Province} from './Components'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () =>{
+  return(
+    <div className="main">
+    <Router>
+      <div>
+        <Header />
+        <div className="main-nav-container">
+          <nav align="center">
+            <table align="center">
+              <tr className="link1">
+                <th className="link1">
+                  <Link to="/International">
+                    International
+                  </Link>
+                </th>
+                <th className="link2">
+                  <Link to="/Local">
+                    Local
+                  </Link>
+                </th>
+                <th className="link3">
+                  <Link to="/Province">
+                    Province
+                  </Link>
+                </th>
+              </tr>
+            </table>
+          </nav>
+        </div>
+      </div>
+      <Switch>
+        <Route path="/International">
+          <International />
+        </Route>
+        <Route path="/Local">
+          <Local />
+        </Route>
+        <Route path="/Province">
+          <Province />
+        </Route>
+      </Switch>
+    </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
