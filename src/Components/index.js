0ac488f1554d572a3625a18868e3d86c36adc676
bryanakaclay <js.bryann@gmail.com>
2020-12-09 23:28:04 +0800

    Replace-Error-Fix

--- src/Components/index.js
+++
@@ -1,6 +0,0 @@
-import Header from './Header'
-import International from './International'
-import Local from './Local'
-import Province from './Province'
-
-export {Header, International, Local, Province}
